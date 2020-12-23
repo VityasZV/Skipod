3227abb6edf5a9df0c13e32c0ab63d558ef62a01
VityasZV <viktor.zadyabin@mail.ru>
2020-12-23 21:19:36 +0300

    fix

--- First/main.cpp
+++ First/main.cpp
@@ -32,7 +32,7 @@ int main (int argc, char* argv[])
             }
             std::cout << "after send" << std::endl; 
             for (int j = 1; j <= cuorum_write; ++j) {
-                MPI_Recv(&file_version_and_process[j-1], 2, MPI_INT, j, 0, MPI_COMM_WORLD, &status); //getting file_versions and ids of processes
+                MPI_Recv(&file_version_and_process[0], 2, MPI_INT, j, 0, MPI_COMM_WORLD, &status); //getting file_versions and ids of processes
                 std::cout << "received " << file_version_and_process[0] << " " << file_version_and_process[1] << std::endl;
                 file_versions_and_proceses[j-1] = file_version_and_process;
                 std::cout << "received " << file_versions_and_proceses[j-1][0] << " " << file_versions_and_proceses[j-1][1] << std::endl;
